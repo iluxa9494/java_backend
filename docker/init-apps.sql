864550d504bb2dab4761857e13223da24cb133ff
Ilia Murashkin <iluxa9494@gmail.com>
2026-01-15 18:26:01 +0300

    доработки

--- docker/init-apps.sql
+++ docker/init-apps.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE currency;
 CREATE DATABASE hotel_booking;
 CREATE DATABASE cryptobot;
+CREATE DATABASE tariff_calculator;
 
 CREATE USER hotel_admin WITH PASSWORD 'postgres';
 ALTER DATABASE hotel_booking OWNER TO hotel_admin;
 
 CREATE USER cryptobot WITH PASSWORD 'cryptobot_password_123';
 ALTER DATABASE cryptobot OWNER TO cryptobot;
+
+CREATE USER tariff_calculator WITH PASSWORD 'tariff_calculator_password';
+ALTER DATABASE tariff_calculator OWNER TO tariff_calculator;
