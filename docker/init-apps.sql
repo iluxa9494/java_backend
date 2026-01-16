ee32a64b27c899fee4a925937d8f629c22bec413
Ilia Murashkin <iluxa9494@gmail.com>
2026-01-16 11:07:42 +0300

    доработки

--- docker/init-apps.sql
+++ docker/init-apps.sql
@@ -18,6 +18,11 @@ BEGIN
   END IF;
   ALTER DATABASE hotel_booking OWNER TO hotel_admin;
 
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'currency_exchange') THEN
+    CREATE USER currency_exchange WITH PASSWORD 'currency_exchange_password';
+  END IF;
+  ALTER DATABASE currency OWNER TO currency_exchange;
+
   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'cryptobot') THEN
     CREATE USER cryptobot WITH PASSWORD 'cryptobot_password_123';
   END IF;
@@ -37,3 +42,11 @@ GRANT CONNECT ON DATABASE tariff_calculator TO tariff_calculator;
 GRANT USAGE, CREATE ON SCHEMA public TO tariff_calculator;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO tariff_calculator;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO tariff_calculator;
+
+GRANT CONNECT ON DATABASE currency TO currency_exchange;
+
+\connect currency
+
+GRANT USAGE, CREATE ON SCHEMA public TO currency_exchange;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO currency_exchange;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO currency_exchange;
