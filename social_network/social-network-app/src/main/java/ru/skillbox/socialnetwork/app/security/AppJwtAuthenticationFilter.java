231533c82355ebc6c6c9f024bbe23ef9c2d38074
Ilia Murashkin <iluxa9494@gmail.com>
2026-02-02 20:20:02 +0300

    доработки

--- social_network/social-network-app/src/main/java/ru/skillbox/socialnetwork/app/security/AppJwtAuthenticationFilter.java
+++ social_network/social-network-app/src/main/java/ru/skillbox/socialnetwork/app/security/AppJwtAuthenticationFilter.java
@@ -5,8 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -23,13 +23,17 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.UUID;
 
-@Slf4j
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE + 20)
-@RequiredArgsConstructor
 public class AppJwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private static final Logger log = LoggerFactory.getLogger(AppJwtAuthenticationFilter.class);
+
     private final AuthTokenService authTokenService;
+
+    public AppJwtAuthenticationFilter(AuthTokenService authTokenService) {
+        this.authTokenService = authTokenService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
