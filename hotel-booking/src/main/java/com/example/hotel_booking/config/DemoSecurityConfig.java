3d8b3af3fcdcda25935007fe6895a7b1988d4bfa
Ilia Murashkin <iluxa9494@gmail.com>
2026-02-05 17:47:47 +0300

    доработки

--- hotel-booking/src/main/java/com/example/hotel_booking/config/DemoSecurityConfig.java
+++ hotel-booking/src/main/java/com/example/hotel_booking/config/DemoSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -24,5 +26,10 @@ public class DemoSecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
