8291a4d18f37566dca31fe7367b9af0cbfdd80ba
Ilia Murashkin <iluxa9494@gmail.com>
2026-02-05 11:29:46 +0300

    доработки

--- tariff_calculator/app/src/main/java/ru/fastdelivery/Main.java
+++ tariff_calculator/app/src/main/java/ru/fastdelivery/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.fastdelivery.config.TariffProperties;
 import ru.fastdelivery.presentation.config.GeoProperties;
 
@@ -13,6 +14,7 @@ import ru.fastdelivery.presentation.config.GeoProperties;
 @SpringBootApplication(scanBasePackages = { "ru.fastdelivery" })
 @ConfigurationPropertiesScan(value = { "ru.fastdelivery.properties" })
 @EnableConfigurationProperties({GeoProperties.class, TariffProperties.class})
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
